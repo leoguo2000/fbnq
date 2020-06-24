7737233b5dd14af850a7ce848fd713e99a1234aa
郭致远 <@LAPTOP-U78I01AP>
2020-06-24 22:52:48 +0800

    test

---
+++ src/fbnq/test.java
@@ -0,0 +1,14 @@
+package fbnq;
+import java.lang.*;
+class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("hahah this is fbnq");
+		
+		
+		
+	}
+
+}
